dc8b181c8dac58200051d67b16e26781f68a110d
oluwasefunmi4 <oluwasefunmi@dojah.io>
2025-08-14 18:54:23 +0100

    fix toml format

--- broke_recipe_streamlit_v2.py
+++ broke_recipe_streamlit_v2.py
@@ -21,8 +21,22 @@ user_id = ''
 # Google Sheets setup
 SCOPE = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
+creds_dict = {
+    "type": st.secrets["type"],
+    "project_id": st.secrets["project_id"],
+    "private_key_id": st.secrets["private_key_id"],
+    "private_key": st.secrets["private_key"],
+    "client_email": st.secrets["client_email"],
+    "client_id": st.secrets["client_id"],
+    "auth_uri": st.secrets["auth_uri"],
+    "token_uri": st.secrets["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+    "universe_domain": st.secrets["universe_domain"]
+}
+
+
 # Load service account from secrets
-creds_dict = json.loads(st.secrets["SERVICE_ACCOUNT_JSON"])
 CREDS = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, SCOPE)
 GSHEET_CLIENT = gspread.authorize(CREDS)
 SHEET = GSHEET_CLIENT.open("RecipeAppLog").sheet1
